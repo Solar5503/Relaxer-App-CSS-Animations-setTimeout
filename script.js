704ed9388383aad5602c030149b4bfd4587d53c9
Solar5503 <spidervvv@mail.ru>
2021-12-08 12:35:54 +0300

    Add music functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+const audio = document.getElementById('audio');
+const playBtn = document.getElementById('play');
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
+let isPlaying = false;
 
 breathAnimation();
 
@@ -25,3 +28,25 @@ function breathAnimation() {
 setInterval(() => {
   breathAnimation();
 }, totalTime);
+
+function audioPlay() {
+  isPlaying = true;
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  audio.volume = 0.7;
+  audio.play();
+}
+
+function audioPause() {
+  isPlaying = false;
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  audio.pause();
+}
+
+playBtn.addEventListener('click', () => {
+  if (isPlaying) audioPause();
+  else audioPlay();
+});
+//if music ended then play music again
+audio.addEventListener('ended', audioPlay);
